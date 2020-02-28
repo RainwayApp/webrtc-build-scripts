11a3dbf8b2f0680339b9d063fabbfff1abaa5838
Roman Ryltsov <roman@alax.info>
2020-02-28 15:14:17 +0200

    Updated build script and ignore list

--- build.ps1
+++ build.ps1
@@ -1,4 +1,13 @@
-$windows_builds = "$PSScriptRoot\win_builds"
+$webrtc_head = "m79"                            # WebRTC branch
+$checkout_path = "$(Get-Location)\webrtc"       # Checkout path
+$windows_builds = "$PSScriptRoot\win_builds"    # Build path
+
+$do_checkout = $true # Checks Depot Tools, checks out webrtc repository; skip if you have this part ready and you are debugging
+
+# TODO: Rename cmdlets using approved verb names, see https://docs.microsoft.com/en-us/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands?view=powershell-7
+
+# WARN: webrtc repo is pretty deep and effective file names are long; you are interested in using as short base path as possible
+
 function Depot-Tools-Installed {
     if (Get-Command "gclient" -ErrorAction SilentlyContinue) {
         return $True
@@ -32,7 +41,7 @@ function Build-WebRTC {
     $build_name = If ($IsDebug) { "Debug" } Else { "Release" }
     $output = "$windows_builds\$Target\$build_name"
     $symbol_level = If ($IsDebug) { 2 } Else { 0 }
-    $arguments = "is_clang=false use_rtti=true is_debug=$is_debug target_cpu=""""$Target"""" symbol_level=$symbol_level rtc_enable_sctp=true fatal_linker_warnings=false treat_warnings_as_errors=false rtc_include_tests=false"
+    $arguments = "use_rtti=true is_debug=$is_debug target_cpu=""""$Target"""" symbol_level=$symbol_level rtc_enable_sctp=true rtc_include_tests=false"
     $gn = Start-Process "gn" -ArgumentList "gen $output --args=""$arguments""" -PassThru -Wait -NoNewWindow
     if ($gn.ExitCode -ne 0) {
         throw "Failed to generate build manifest for $output"
@@ -72,74 +81,74 @@ function Copy-Includes {
     }
     Write-Output "Copying Includes"
     xcopy /s "$(Get-Location)\*.h" "$includes_path" /Q
-    xcopy /s "$(Get-Location)\third_party\abseil-cpp\absl\*.h"  "$includes_path\absl\" /Q
-    #Remove-Item -Path "$includes_path\third_party" -Force -Recurse
+    # NOTE: We keep third_party because we need its third_party\abseil-cpp\absl at the very least
+    # Remove-Item -Path "$includes_path\third_party" -Force -Recurse
 }
 
-$depot_path = "$(Get-Location)\depot_tools"
-$Env:Path += ";$depot_path"
-$webrtc_head = "m76"
+if($do_checkout) {
 
+    if ((Depot-Tools-Installed) -eq $false) {
+        $download_path = "$(Get-Location)/depot_tools"
+        $zip_path = Join-Path -Path $download_path -ChildPath "depot_tools.zip"
+        New-Item -ItemType Directory -Path $download_path -Force | Out-Null
+        Write-Output "Downloading depot tools to $zip_path..."
+        (new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/chrome-infra/depot_tools.zip', $zip_path);
+        Write-Output "Extracting depot tools to $download_path..."
+        Expand-Archive $zip_path -DestinationPath $download_path -Force
+        Write-Output "Deleting $zip_path..."
+        Remove-Item -Path $zip_path -Force -ErrorAction Ignore
+        Write-Output $download_path
+        $Env:Path += ";$(Get-Location)\depot_tools" # With pre-existing Depot Tools it is assumed path is OK; also it is recommended to have this path at the head of the list
+    }
 
-if ((Depot-Tools-Installed) -eq $false) {
-    $download_path = "$(Get-Location)/depot_tools"
-    $zip_path = Join-Path -Path $download_path -ChildPath "depot_tools.zip"
-    New-Item -ItemType Directory -Path $download_path -Force | Out-Null
-    Write-Output "Downloading depot tools to $zip_path..."
-    (new-object System.Net.WebClient).DownloadFile('https://storage.googleapis.com/chrome-infra/depot_tools.zip', $zip_path);
-    Write-Output "Extracting depot tools to $download_path..."
-    Expand-Archive $zip_path -DestinationPath $download_path -Force
-    Write-Output "Deleting $zip_path..."
-    Remove-Item -Path $zip_path -Force -ErrorAction Ignore
-    Write-Output $download_path
-}
+    if (Test-Path $checkout_path) {
+        Write-Output "Cleaning up last checkout..."
+        Remove-Item -LiteralPath $checkout_path -Force -Recurse
+        Write-Output "Done."
+    }
 
-$checkout_path = "$(Get-Location)\webrtc"
+    New-Item -ItemType Directory -Path $checkout_path
+    Set-Location -Path $checkout_path
+    Write-Output "Fetching WebRTC"
 
-if (Test-Path $checkout_path) {
-    Write-Output "Cleaning up last checkout..."
-    Remove-Item -LiteralPath $checkout_path -Force -Recurse
-    Write-Output "Done."
-}
+    $fetch_process = Start-Process "fetch" -ArgumentList "--nohooks webrtc" -PassThru -Wait -NoNewWindow
+    if ($fetch_process.ExitCode -ne 0) {
+        throw "Failed to fetch WebRTC."
+    }
 
-New-Item -ItemType Directory -Path $checkout_path
+    Set-Location -Path "$checkout_path\src"
+    Write-Output "Checking out remote header $webrtc_head..."
 
-Set-Location -Path $checkout_path
-
-Write-Output "Fetching WebRTC"
-
-$fetch_process = Start-Process "fetch" -ArgumentList "--nohooks webrtc" -PassThru -Wait -NoNewWindow
-if ($fetch_process.ExitCode -ne 0) {
-    throw "Failed to fetch WebRTC."
-}
-
-Set-Location -Path "$checkout_path\src"
-
-Write-Output "Checking out remote header $webrtc_head..."
-
-$checkout = Start-Process "git" -ArgumentList "checkout -b spitfire refs/remotes/branch-heads/$webrtc_head" -PassThru -Wait -NoNewWindow
-if ($checkout.ExitCode -ne 0) {
-    throw "Failed to checkout remote WebRTC branch"
+    $checkout = Start-Process "git" -ArgumentList "checkout -b spitfire refs/remotes/branch-heads/$webrtc_head" -PassThru -Wait -NoNewWindow
+    if ($checkout.ExitCode -ne 0) {
+        throw "Failed to checkout remote WebRTC branch"
+    }
 }
 
 Write-Output "Syncing repo..."
+Set-Location -Path "$checkout_path\src"
+
+$Env:DEPOT_TOOLS_WIN_TOOLCHAIN=0 # Use of locally installed Visual Studio
+$python_lastchange = Start-Process "python" -ArgumentList "build/util/lastchange.py -o build/util/LASTCHANGE" -PassThru -Wait -NoNewWindow
+if ($python_lastchange.ExitCode -ne 0) {
+    throw "Failed to generate lastchange"
+}
 
 $gclient = Start-Process "gclient" -ArgumentList "sync" -PassThru -Wait -NoNewWindow
 if ($gclient.ExitCode -ne 0) {
     throw "Failed to sync WebRTC code. $($gclient.ExitCode)"
 }
 
-Write-Output "Applying patches to WebRTC..."
-Apply-Patch -Name "$PSScriptRoot\patches\optimize.patch"
-Write-Output "Applying patches to usrsctp..."
-Set-Location -Path "$checkout_path\src\third_party\usrsctp\usrsctplib"
-Apply-Patch -Name "$PSScriptRoot\patches\usrsctp.patch"
-Write-Output "Done patching."
+Write-Output "Applying webrtc patches..."
 Set-Location -Path "$checkout_path\src"
+Apply-Patch -Name "$PSScriptRoot\patches\webrtc-src\optimize.patch"
+Write-Output "Applying webrtc-src-build patches..."
+Set-Location -Path "$checkout_path\src\build"
+Apply-Patch -Name "$PSScriptRoot\patches\webrtc-src-build\0001-Spitfire-C-CLI-build-customization.patch"
+Apply-Patch -Name "$PSScriptRoot\patches\webrtc-src-build\0002-Do-not-use-custom-in-tree-libc-for-Windows-clang-bui.patch"
+Write-Output "Done patching."
 
-Write-Output "Replacing BUILD.gn"
-Copy-Item "$PSScriptRoot\configs\BUILD.gn" -Destination "$(Get-Location)\build\config\win\BUILD.gn" -force
-
+Set-Location -Path "$checkout_path\src"
 
 if (Test-Path $windows_builds) {
     Write-Output "Cleaning up old builds..."
@@ -164,6 +173,5 @@ Copy-Libs -Target "x86" -IsDebug $true
 Copy-Libs -Target "x86" -IsDebug $false
 
 Copy-Includes
-
 
 Write-Output "WebRTC Built!"
